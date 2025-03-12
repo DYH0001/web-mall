663288dad690ad7dec24cb4275385759371eb723
dyh64 <276179395@qq.com>
2025-03-12 17:00:51 +0800

    支付服务接口

--- kejizhai/src/study/kejizhai/services/ItemService.java
+++ kejizhai/src/study/kejizhai/services/ItemService.java
@@ -1,11 +1,11 @@
 import study.kejizhai.bean.*;
 
 public interface ItemService {
-    public Items getItem(int iid) throws SqlException;
-    public boolean addItem(Items items) throws SqlException;
-    public boolean updateItem(Items items) throws SqlException;
-    public boolean deleteItem(int iid) throws SqlException;
-    public List<Items> searchItems(String keyword) throws SqlException;
+    public List<Items> getItem(int iid) throws SqlException; //search
+    public boolean addItem(Items items) throws SqlException;  //上架
+    public boolean updateItem(Items items) throws SqlException; //更新
+    public boolean deleteItem(int iid) throws SqlException; //下架
+    
     public List<Items> getItems() throws SqlException;
     public int setIstatus(int iid);
 }
--- kejizhai/src/study/kejizhai/services/paymentService.java
+++ kejizhai/src/study/kejizhai/services/paymentService.java
@@ -5,9 +5,8 @@ import study.kejizhai.bean.payment;
 import java.util.List;
 
 public interface PaymentService {
-    PaymentResponse pay(PaymentRequest request) throws PaymentException;
-    PaymentNotifyResult handleNotify(Map<String, String> params);
-    
-    PaymentStatus queryStatus(String orderId);
+    public boolean checkPayment(String paymentId, String userId, String amount, String paymentType, String paymentStatus);
+
+    public boolean 
 }
 
