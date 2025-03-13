8a49f32984b516bb5a8221c8594cb87181e572d4
dyh0001 <dyh276179395@outlook.com>
2025-03-13 18:47:19 +0800

    springCconfig修改

--- kejizhai/src/main/java/study/kejizhai/config/SpringConfig.java
+++ kejizhai/src/main/java/study/kejizhai/config/SpringConfig.java
@@ -1,10 +1,30 @@
 package study.kejizhai.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.jdbc.core.JdbcTemplate;
+import javax.sql.DataSource;
 
 @Configuration
 @ComponentScan("study.kejizhai")
+@EnableTransactionManagement
 public class SpringConfig {
-    // 配置类的其他内容
+    
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/kejizhai?useSSL=false&serverTimezone=UTC");
+        dataSource.setUsername("root");
+        dataSource.setPassword("dyhDYH@040127");
+        return dataSource;
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate(DataSource dataSource) {
+        return new JdbcTemplate(dataSource);
+    }
 }
