b4dff5851e14e837eca0139ab8e4f57898d24fb3
dyh64 <276179395@qq.com>
2025-03-14 14:24:57 +0800

    OrderService

--- kejizhai/src/main/java/study/kejizhai/services/OrderService.java
+++ kejizhai/src/main/java/study/kejizhai/services/OrderService.java
@@ -10,7 +10,8 @@ import java.util.List;
 public interface OrderService {
     public List<Order> getOrders(String uid) throws SQLException;
     public boolean addOrder(Order orders) throws SQLException;
-    public boolean deleteOrder(String oid) throws SQLException;
     public boolean changeOrder(Order orders) throws SQLException;
     public List<Order> searchOrders(String keyword) throws SQLException;
+
+    public boolean deleteOrder(String oid) throws SQLException;
 }
